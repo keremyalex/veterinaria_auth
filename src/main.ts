1aa2f0005ae323f83d611b743fbea6eada31ad9b
alex keremy <keremy.alex@gmail.com>
2025-10-31 16:45:35 -0400

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3001'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With', 'apollo-require-preflight']
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
